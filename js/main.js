20925bb09f5476dcbfc7fe3e74ffc65e6d167d38
Francisco Atozqui <franciscoatozqui@yahoo.com.ar>
2022-10-25 11:16:48 -0300

    segundo commit

--- js/main.js
+++ js/main.js
@@ -1,22 +1,19 @@
 const camisetas = [
     { pais: "argentina", precio: 4000, imagen: "argentina.jpg"},
-    { pais: "brasil", precio: 3500, imagen: "brasil.jpg" },
     { pais: "alemania", precio: 3800, imagen: "alemania.jpg" },
-    { pais: "portugal", precio: 3200, imagen: "portugal.jpg" },
-    { pais: "uruguay", precio: 2800, imagen: "uruguay.jpg" },
     { pais: "belgica", precio: 3000, imagen: "belgica.jpg" },
-    { pais: "inglaterra", precio: 3400, imagen: "inglaterra.jpg" },
+    { pais: "brasil", precio: 3500, imagen: "brasil.jpg" },
     { pais: "francia", precio: 3700, imagen: "francia.jpg" },
+    { pais: "inglaterra", precio: 3400, imagen: "inglaterra.jpg" },
+    { pais: "uruguay", precio: 2800, imagen: "uruguay.jpg" },
+    { pais: "portugal", precio: 3200, imagen: "portugal.jpg" },
 ]
 
-const carrito = [];
-const productosCarrito = document.que
-const btnCarrito = document.querySelector("#btnCarrito");
-const inputBuscar = document.querySelector("#buscarProducto");
+
+let inputBuscar = document.querySelector("#buscarProducto");
 const btnSearch = document.querySelector("#btnSearch");
 const cards = document.querySelector("#productosBuscados");
-
-
+const btnCarrito = document.querySelector("#btnCarrito");
 
 function buscarProducto(arr, filtro) {
     const encontrarProducto = arr.find((el) => {
@@ -32,8 +29,9 @@ function filtrarProducto(arr, filtro) {
     return filtrado;
 }
 
-btnSearch.addEventListener('click', () => {
-    let busqueda = filtrarProducto(camisetas, inputBuscar.value.toLowerCase())
+inputBuscar.addEventListener('input', (e) => {
+    e.preventDefault();
+    let busqueda = filtrarProducto(camisetas, inputBuscar.value.toLowerCase());
     cards.innerHTML = `
     <section class="container" id="container-cards">
         <div class="row cards-container d-flex justify-content-center">
@@ -41,20 +39,80 @@ btnSearch.addEventListener('click', () => {
                 <img src="../img/${busqueda[0].imagen}" class="card-img-top" alt="">
                 <div class="card-body">
                 <h5 class="card-title">Camiseta de ${busqueda[0].pais}</h5>
-                    <p class="card-text">Precio: ${busqueda[0].precio}</p>
-                    <a href="#" class="btn btn-primary" id="btnCarrito">Agregar al carrito</a>
+                <p class="card-text">Precio: ${busqueda[0].precio}</p>
+                <a href="" class="btn btn-primary bg-dark" id="btnCarrito">Agregar al carrito <i class="fa-solid fa-cart-shopping"></i></a>
             </div>
         </div>
         </div>
     </section>`
 })
+const guardar = document.querySelector("#guardarValor")
+
+localStorage.setItem('clave', JSON.stringify(inputBuscar.value.toLowerCase()));
 
 btnCarrito.addEventListener('click', () => {
-    
+    let guardarProductoEnCarrito = localStorage.getItem('clave');
+    return guardarProductoEnCarrito;
 })
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function guardarProductos(carrito) {
+//     busqueda = filtrarProducto(camisetas, inputBuscar.value.toLowerCase());
+//     if (carrito === "localStorage") {
+//         localStorage.setItem('clave', JSON.stringify(inputBuscar.value.toLowerCase()));
+//     } else if (carrito === "sessionStorage"){
+//         sessionStorage.setItem('clave', JSON.stringify(inputBuscar.value.toLowerCase()));
+//     }
+// }
+
+// guardar.addEventListener('click', (e) => {
+//     e.preventDefault();
+//         let productoGuardado = guardarProductos("localStorage");
+//         return productoGuardado;
+//     })
+
+
+
+
+
+
+
+
+
+
+
     
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // const nombre = document.querySelector("#name"),
 //     nombreUsuario = document.querySelector("#user"),
@@ -62,6 +120,11 @@ btnCarrito.addEventListener('click', () => {
 //     password = document.querySelector("#password"),
 //     enviar = document.querySelector("#enviar"),
 // const datosUsuario = { usuario: email.value, pass: password.value };
+
+
+
+
+
 
 
 // inputBuscar.addEventListener("change", () => {
@@ -72,32 +135,32 @@ btnCarrito.addEventListener('click', () => {
 //     item.pais.includes(inputBuscar.value.toUpperCase())
 // );
 
-// function buscarProducto(arr, filtro) {
-//     const encontrar = arr.find((el) => {
-//         return el.pais.includes(filtro);
-//     });
-//     return encontrar;
-// }
-
-// function filtrarProducto(arr, filtro) {
-//     const filtrar = arr.filter((el) => {
-//         return el.pais.includes(filtro);
-//     });
-//     return filtrar;
-// }
-
-// enviar.addEventListener('click', () => {
-//     let resultado = filtrarProducto(camisetas, btnSearch.value.toLowerCase())
-//     parrafo[0].innerHTML = `
-//   <h3>${resultado[0].pais}</h3>
-//   <p>${resultado[0].precio}</p>`
-// })
 
 
-// const formulario = document.querySelector("form");
-// formulario.addEventListener('submit', (e) => {
-//     e.preventDefault();
-// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
